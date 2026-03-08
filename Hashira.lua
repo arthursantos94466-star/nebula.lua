4fe001e23cf46a702baf8a8b747a6fed91247088
arthursantos94466-star <arthursantos94466@gmail.com>
2026-03-08 01:22:20 -0300

    Hashira.lua

--- Hashira.lua
+++ Hashira.lua
@@ -1,5 +1,5 @@
 -- [[ CONFIGURAÇÃO E BOTÃO MOBILE ]] --
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Nebula Hub | V6 ULTRA",
